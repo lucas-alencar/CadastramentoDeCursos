51148bbcca7e3600b74f93c91441193d97755eba
lucas-alencar <lucasalencar.rd@gmail.com>
2023-09-02 17:59:01 -0300

    Adding initial screen

---
+++ sistema-front/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,30 @@
+import { CoursesService } from './../services/courses.service';
+import { Component } from '@angular/core';
+import { Course } from '../model/course';
+import { Observable, catchError, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent {
+  courses$: Observable<Course[]>;
+  displayedColumns = ['name', 'category'];
+  constructor(
+    private CoursesService: CoursesService,
+    public dialog: MatDialog){
+    this.courses$ = this.CoursesService.list()
+    .pipe(catchError(error => {
+      this.onError('Erro ao carregar informações dos cursos');
+      return of([]);
+    }));
+  }
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
+  }
+}
