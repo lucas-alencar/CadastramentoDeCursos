660702b76f5768bb223c686e3d15d9ecaf433a33
lucas-alencar <lucasalencar.rd@gmail.com>
2023-09-02 21:25:42 -0300

    creating Spring project

---
+++ sistema-back/src/main/java/com/lucasalencar/sistemaback/SistemaBackApplication.java
@@ -0,0 +1,13 @@
+package com.lucasalencar.sistemaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBackApplication.class, args);
+	}
+
+}
---
+++ sistema-back/src/test/java/com/lucasalencar/sistemaback/SistemaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasalencar.sistemaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
