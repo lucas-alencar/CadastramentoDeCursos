2b53491c6becd188dea81b1e818e422f8c0b6c3a
lucas-alencar <lucasalencar.rd@gmail.com>
2023-09-04 08:55:37 -0300

    adding Course in back-end

--- sistema-back/src/main/java/com/lucasalencar/sistemaback/SistemaBackApplication.java
+++ sistema-back/src/main/java/com/lucasalencar/sistemaback/SistemaBackApplication.java
@@ -1,7 +1,12 @@
 package com.lucasalencar.sistemaback;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.lucasalencar.sistemaback.model.Course;
+import com.lucasalencar.sistemaback.repository.CourseRepository;
 
 @SpringBootApplication
 public class SistemaBackApplication {
@@ -9,5 +14,17 @@ public class SistemaBackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaBackApplication.class, args);
 	}
+	
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository){
+		return args -> {
+			courseRepository.deleteAll();
 
+			Course c = new Course();
+			c.setName("connection angular spring");
+			c.setCategory("back");
+
+			courseRepository.save(c);
+		};
+	}
 }
